f22984fae39aa99e2cc0b082b0f31e338dfe880e
21shawnm17 <17shawnm@gmail.com>
2017-01-14 09:49:33 -0500

    I added to the isfinished and execute classes

--- src/org/usfirst/frc/team614/robot/commands/autonomous/DeliverCenterGear.java
+++ src/org/usfirst/frc/team614/robot/commands/autonomous/DeliverCenterGear.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team614.robot.commands.autonomous;
 
+import org.usfirst.frc.team614.robot.Robot;
+import org.usfirst.frc.team614.robot.subsystems.Drivetrain;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,20 +14,33 @@ public class DeliverCenterGear extends Command {
     public DeliverCenterGear() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.drivetrain);
     }
+   
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    Robot.resetEncoder();
+    	
+    
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    Robot.encoder.getDistance();
+    
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
-    }
+        if (Robot.encoder != null){
+        	Robot.encoder.getDistance();}
+     
+        	
+        }
+        	
+    
 
     // Called once after isFinished returns true
     protected void end() {
