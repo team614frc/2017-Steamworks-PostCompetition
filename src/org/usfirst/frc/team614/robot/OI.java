5371ea9f295a6edc9a1ada4687fa30754f2b82f9
team614 <team614robotics@gmail.com>
2017-03-13 16:07:08 -0400

    Cleaned OI a little (actually a test commit)

--- src/org/usfirst/frc/team614/robot/OI.java
+++ src/org/usfirst/frc/team614/robot/OI.java
@@ -48,20 +48,18 @@ public class OI {
 	// X-Box controller(s)
 	public static final Gamepad driverGamepad = new Gamepad(0);
 	public static final Gamepad operatorGamepad = new Gamepad(1);
+
 	
-//	private static final Button zeroNavxYaw = new JoystickButton(driverGamepad, Gamepad.button_Back);
-//	private static final Button driveAtSmartDashboardSpeed = new JoystickButton(driverGamepad, Gamepad.button_Y);
+	private static final Button revElevator = new JoystickButton(driverGamepad, Gamepad.button_L_Shoulder);
+
+	
 	private static final Button tryToClimbRope = new JoystickButton(operatorGamepad, Gamepad.button_R_Shoulder);
 	private static final Button stopClimber = new JoystickButton(operatorGamepad, Gamepad.button_L_Shoulder);
 	private static final Button reverseClimber = new JoystickButton(operatorGamepad, Gamepad.button_Back);
 	
-	private static final Button revElevator = new JoystickButton(driverGamepad, Gamepad.button_L_Shoulder);
-//	private static final Button revShooterFromBoiler = new JoystickButton(driverGamepad, Gamepad.button_Y);
-//	private static final Button revShooterFromAirship = new JoystickButton(driverGamepad, Gamepad.button_X);
 	private static final Button staticRevShooterFromBoiler = new JoystickButton(operatorGamepad, Gamepad.button_B);
 	private static final Button increaseShooterSpeed = new JoystickButton(operatorGamepad, Gamepad.button_Y);
 	private static final Button decreaseShooterSpeed = new JoystickButton(operatorGamepad, Gamepad.button_A);
-//	private static final Button staticRevShooterFromAirship = new JoystickButton(driverGamepad, Gamepad.button_X);
 
 //	private static final Button deliverGear = new JoystickButton(driverGamepad, Gamepad.button_A);
 
@@ -73,14 +71,10 @@ public class OI {
 		stopClimber.whenPressed(new StopWinch());
 		reverseClimber.whileHeld(new ReverseWinch());
 		revElevator.whileHeld(new RevElevator());
-//		deliverGear.whenPressed(new DeliverGear(false, false));
-//		revShooterFromBoiler.whileHeld(new Shoot(false, true, false, false));
-//		revShooterFromAirship.whileHeld(new Shoot(true, true, false, false));
 		
 		staticRevShooterFromBoiler.whileHeld(new Shoot(true, false, false, false, false));
 		increaseShooterSpeed.whenPressed(new ChangeSpeedBy10Percent(true));
 		decreaseShooterSpeed.whenPressed(new ChangeSpeedBy10Percent(false));
-//		staticRevShooterFromAirship.whileHeld(new Shoot(true, true, false, false, false));
 		
 	}
 }
